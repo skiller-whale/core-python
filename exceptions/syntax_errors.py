cc97d3546cd1f32e080989c9255052098bccbd2e
Dave Millican <dave@skillerwhale.com>
2021-05-20 11:56:07 +0100

    Add exercises for exceptions (#4)

---
+++ exceptions/syntax_errors.py
@@ -0,0 +1,15 @@
+"""
+The code block below contains several SyntaxErrors. Run this python file (with
+python3) and use the SyntaxError messages to identify and fix the errors.
+
+When this code is working, it should print out the numbers 1 to 5, along with
+their squares (n * n) on the line below.
+"""
+
+
+for i in range(1, 6) {
+     square = i ** 2
+    print(str(i), "Squared is:)
+	print(square))
+     print()
+}
