8f3c9cc7831196ab845a89240c7df2d6e4c76851
Dave Millican <dave@skillerwhale.com>
2021-10-07 12:47:01 +0100

    Add intro_to_python exercises

---
+++ intro_to_python/exercises.py
@@ -0,0 +1,63 @@
+"""An introduction to some of the fundamentals of Python"""
+
+"""
+STRINGS
+~~~~~~~
+"""
+
+print('Hello there')
+print('There isnt one right way of doing this')
+
+
+"""
+VARIABLES
+~~~~~~~~~
+"""
+
+first = "2nd"
+second = "1st"
+
+# TODO: Without changing the code above, add new code that swaps the values of
+# the variables first and second, so first is "1st" and second is "2nd"
+
+# print("\nfirst is", first, "\nsecond is", second)
+
+
+"""
+ARITHMETIC
+~~~~~~~~~~
+"""
+
+# print()
+
+height1 = 184
+height2 = 156
+height3 = 176
+# print("The average height is", "???")
+
+minutes_to_play_one_game = 17
+total_minutes_available = 325
+# print("The number of games that can be completed is", "???")
+# print("The number of spare minutes left over is", "???")
+
+
+"""
+NUMERIC TYPES
+~~~~~~~~~~~~~
+"""
+# print()
+
+one_over_one = 1/1
+
+# print("The type of 1/1 is:", "???")
+
+
+"""
+CONVERTING BETWEEN TYPES
+~~~~~~~~~~~~~~~~~~~~~~~~
+"""
+# print()
+
+two = "2.0"
+four = "4"
+# print("2 + 4 equals " + two + four)
