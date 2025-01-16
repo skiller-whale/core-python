650a1cb5dbf9496c6728d7f74a7d1668b9eca069
Traiko Dinev <traiko.dinev@gmail.com>
2025-01-16 14:35:20 +0000

    dicts, looping function exercise fixes

--- exercises/looping_functions/exercise_4.py
+++ exercises/looping_functions/exercise_4.py
@@ -6,13 +6,14 @@ UNZIPPING
 ---------
 
 * Using `words_and_frequencies`, the `filter` function, and unzipping, print out
-    the words with a frequency under 5,000,000.
+    the words with a frequency under 5_060_000 (`frequency_threshold`).
 
 HINT: You can use filter(function, collection) to return items from a `collection`
     for which `function(item)` is True
 """
 
 words_and_frequencies = zip(words, word_frequencies)
+frequency_threshold = 5_060_000
 
 filtered_pairs = ...
 filtered_words = ...
